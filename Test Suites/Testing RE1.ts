86ecf05a528dd801db192d798a0c81cb7adfa2a2
shivam23shah <sshah@lrescorp.com>
2020-01-17 16:13:54 -0800

    <TestCaseAddedandObjectRepo>

--- Test Suites/Testing RE1.ts
+++ Test Suites/Testing RE1.ts
@@ -11,9 +11,9 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>b0aa18d0-ac34-4677-966f-a9bfae54802c</testSuiteGuid>
    <testCaseLink>
-      <guid>ad1db6c5-a443-4325-b3e5-40e9f8523587</guid>
+      <guid>d98a1bed-66e3-4735-95b2-28c0ef3ed7c9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Testing</testCaseId>
+      <testCaseId>Test Cases/Website</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
